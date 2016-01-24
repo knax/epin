ebbfca91233e29a0813cbcfffa99c679601a1121
Hasta Ragil <npcmamaz@gmail.com>
2016-01-24 15:49:22 +0700

    add compiled version

--- resources/views/index-angular.php
+++ resources/views/index-angular.php
@@ -5,8 +5,8 @@
   <title>Diary</title>
   <base href="/">
   <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimal-ui" />
-<link href="/app.df2ce709ff855abff87f.css" rel="stylesheet"></head>
+<link href="/app.19568146785b99495d2e.css" rel="stylesheet"></head>
 <body>
 <ui-view></ui-view>
-<script src="/app.df2ce709ff855abff87f.js"></script></body>
+<script src="/app.19568146785b99495d2e.js"></script></body>
 </html>
